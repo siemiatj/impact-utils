113a2a99dfefcec97c4765d027053cc68e0e16ba
jakub <jakub@bryntum.com>
2013-06-14 16:19:53 +0200

    - initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ impact-utils
 ============
 
 Small utils and addons for Impact.js game engine.
+
+Feel free to use and distribute it.
---
+++ input.js
@@ -0,0 +1,43 @@
+/**
+ * Module adding additional functionalities to the base Input class of Impact.
+ *
+ * @author
+ * sasklacz : kuba@virtualdesign.pl
+ * github.com/siemiatj
+ * 
+ **/
+ig.module(
+    //you need to rename the module name to fit your naming pattern
+    'input'
+)
+.requires(
+    'impact.input'
+).
+defines(function() {
+
+    ig.Input.inject({
+        /**
+         * Function additionally checks if any key is currently pressed
+         **/
+        state: function( action ) {
+            if (!action){
+                return this.anyKeyPressed();
+            }
+            return this.parent(action);
+        },
+
+        /**
+         * Helper function checking if object holding pressed keys is empty
+         **/
+        anyKeyPressed: function() {
+            var obj = this.presses,
+                hasOwnProperty = Object.prototype.hasOwnProperty;
+
+            for (var key in obj) {
+                if (hasOwnProperty.call(obj, key)) return true;
+            }
+
+            return false;
+        }
+    });
+});
